b2e994b2ea0c411a7a9159980f1fe8371ab06a5e
VincentGeng67 <55418598+VincentGeng67@users.noreply.github.com>
2020-07-29 09:57:37 -0400

    Add files via upload
    improve efficient

--- Research/Data/superpixels/HGPmeta.py
+++ Research/Data/superpixels/HGPmeta.py
@@ -16,7 +16,7 @@ class HGPMeta(nn.Module):
     """
     Meta Learner
     """
-    def __init__(self,update_lr,meta_lr,way):
+    def __init__(self,update_lr,meta_lr,way,update_step,update_step_test):
         """
         :param args:
         """
@@ -28,8 +28,8 @@ class HGPMeta(nn.Module):
 #         self.k_spt = args.k_spt
 #         self.k_qry = args.k_qry
 #         self.task_num = args.task_num
-        self.update_step = 70
-        self.update_step_test=70
+        self.update_step = update_step
+        self.update_step_test = update_step_test
         
 #         self.update_step_test = args.update_step_test
 
@@ -37,7 +37,7 @@ class HGPMeta(nn.Module):
         self.net = HGPLearner(way)
 
         
-        self.meta_optim = optim.Adam(self.net.parameters(), lr=meta_lr, weight_decay=0.001)
+        
 
 
 
@@ -80,16 +80,61 @@ class HGPMeta(nn.Module):
 #         print('size',querysz)
         losses_q = [0 for _ in range(self.update_step + 1)]  # losses_q[i] is the loss on step i
         corrects = [0 for _ in range(self.update_step + 1)]
-
-
+        metagrad=[]
+         
         for i in range(task_num):
 
             score,logits,loss,correct= self.net(x_spt[i],'train', vars=None, bn_training=True,init=True)
-
+            del score
+            del logits
+            del correct
        
 
+
+            # grad = torch.autograd.grad(loss, self.net.parameters(),retain_graph=True,create_graph=True)
             grad = torch.autograd.grad(loss, self.net.parameters())
             fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, self.net.parameters())))
+#             enlist=[0,1,3,5,6,7,9,11,12,13]
+#             print('checkeffectbefore',grad[0])  
+#             for l1 in  enlist:
+#                 print('second order',l1)
+#                 length=0
+#                 more=0
+#                 if (l1 ==12):
+#                     length=len(grad[l1]) 
+#                     more=2
+#                 elif (len(grad[l1])>1):
+#                     length=len(grad[l1])
+#                     more=0
+#                 else :
+#                     length=len(grad[l1][0])
+#                     more=1
+#                 for l2 in range(length):
+                    
+#                     if (more==1) :
+# #                         print('element',grad[l1][0][l2])
+#                         s_grads = torch.autograd.grad(grad[l1][0][l2], self.net.parameters(),allow_unused=True,retain_graph=True)
+#                         grad[l1][0][l2]=s_grads[l1][0][l2]
+#                         del s_grads
+#                     elif(more==0):
+# #                         print('element',grad[l1][l2])
+#                         s_grads = torch.autograd.grad(grad[l1][l2], self.net.parameters(),allow_unused=True,retain_graph=True)
+#                         grad[l1][l2]=s_grads[l1][l2]
+#                         del s_grads
+#                     else:
+#                         for l3 in range(len(grad[l1][l2])): 
+# #                             print('element',grad[l1][l2][l3])
+#                             s_grads = torch.autograd.grad(grad[l1][l2][l3], self.net.parameters(),allow_unused=True,retain_graph=True)
+#                             grad[l1][l2][l3]=s_grads[l1][l2][l3]
+#                             del s_grads
+#             print('checkeffect',grad[0])             
+            sgrad = list(map(lambda p: 1 - self.update_lr * p[0], zip(grad)))
+            
+            # f = torch.autograd.grad(loss, self.net.parameters())
+            # del f
+            del grad
+
+            
 
 
             
@@ -98,10 +143,11 @@ class HGPMeta(nn.Module):
                 # [setsz, nway]
 
                 score_q,logits_q,loss_q,correct= self.net(x_qry[i],'test', self.net.parameters(), bn_training=True,init=True)
+                del score_q
+                del logits_q
+                losses_q[0] += float(loss_q)
 
-                losses_q[0] += loss_q
-
-                corrects[0] = corrects[0] + correct
+                corrects[0] = corrects[0] + float(correct)
 # this is the loss and accuracy after the first update
 
             with torch.no_grad():
@@ -110,10 +156,11 @@ class HGPMeta(nn.Module):
                 score_q,logits_q,loss_q,correct= self.net(x_qry[i],'test', fast_weights, bn_training=True)
 #                 loss_q = criterion(score_q, y_qry[i])
 #                 loss_q=mloss_q
-
-                losses_q[1] += loss_q
+                del score_q
+                del logits_q
+                losses_q[1] += float(loss_q)
                 # [setsz]
-                corrects[1] = corrects[1] + correct
+                corrects[1] = corrects[1] + float(correct)
 #                 print('correct',correct)
 
 
@@ -128,38 +175,91 @@ class HGPMeta(nn.Module):
 #                 loss = F.cross_entropy(logits, y_spt[i])
                 score,logits,loss,correct= self.net(x_spt[i],'train', fast_weights, bn_training=True)
 
+                del score
+                del logits
+                del correct
                 
-                
+                # grad = torch.autograd.grad(loss,self.net.modelbn,retain_graph=True,create_graph=True)
                 grad = torch.autograd.grad(loss,self.net.modelbn)
 #                 print('checkgrad1',grad[0])
                 fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, fast_weights)))
-#                 print('fwit',k,fast_weights[0])
-#                 print('sorrypred1',logits)
-#                 print('sorrylabel2',y_spt[i])
+#                 enlist=[0,1,3,5,6,7,9,11,12,13]
+#                 for l1 in  enlist:
+#                     print('second order',l1)
+#                     length=0
+#                     more=0
+#                     if (l1 ==12):
+#                         length=len(grad[l1]) 
+#                         more=2
+#                     elif (len(grad[l1])>1):
+#                         length=len(grad[l1])
+#                         more=0
+#                     else :
+#                         length=len(grad[l1][0])
+#                         more=1
+#                     for l2 in range(length):
+                    
+#                         if (more==1) :
+# #                         print('element',grad[l1][0][l2])
+#                             s_grads = torch.autograd.grad(grad[l1][0][l2], self.net.modelbn,allow_unused=True,retain_graph=True)
+#                             grad[l1][0][l2]=s_grads[l1][0][l2]
+#                             del s_grads
+#                         elif(more==0):
+# #                         print('element',grad[l1][l2])
+#                             s_grads = torch.autograd.grad(grad[l1][l2], self.net.modelbn,allow_unused=True,retain_graph=True)
+#                             grad[l1][l2]=s_grads[l1][l2]
+#                             del s_grads
+#                         else:
+#                             for l3 in range(len(grad[l1][l2])): 
+# #                             print('element',grad[l1][l2][l3])
+#                                 s_grads = torch.autograd.grad(grad[l1][l2][l3], self.net.modelbn,allow_unused=True,retain_graph=True)
+#                                 grad[l1][l2][l3]=s_grads[l1][l2][l3]
+#                                 del s_grads
+                grad = list(map(lambda p: 1 - self.update_lr * p[0], zip(grad)))
+                sgrad= list(map(lambda p: p[0] * p[1], zip(grad,sgrad)))
+                del grad
+                # f = torch.autograd.grad(loss, self.net.parameters())
+                # del f
+
                 score_q,logits_q,loss_q,correct= self.net(x_qry[i],'test', fast_weights, bn_training=True)
-#                 loss_q = criterion(score_q, y_qry[i])
-                losses_q[k + 1] += loss_q
-
-
+                losses_q[k + 1] += float(loss_q)
+    
+                del score_q
+                del logits_q
+            
+                if(k==self.update_step-1):
+                    fgrad=torch.autograd.grad(loss_q, self.net.modelbn)
+                    print('final ratio grad',sgrad[0])
+                    print('final ratio grad',sgrad[13])
+                    sgrad= list(map(lambda p: p[0] * p[1], zip(fgrad,sgrad)))
+                    del fgrad
+                    del fast_weights
+                    metagrad.append(sgrad)
+                    print('final step grad',sgrad[0])
+                    print('final step grad',sgrad[13])
                 with torch.no_grad():
 
-                    corrects[k + 1] = corrects[k + 1] + correct
+                    corrects[k + 1] = corrects[k + 1] + float(correct)
             
 
 
+        up_grad=list(map(lambda p: p[0]/ task_num, zip(metagrad[0])))
+        
+        for tas in range(1,len(metagrad)):
+            up_grad= list(map(lambda p: p[0] + p[1]/ task_num, zip(up_grad,metagrad[tas])))
 
 
-
-        loss_q = losses_q[-1] / task_num
-        print('beforepara',self.net.parameters()[0])
-        self.meta_optim.zero_grad()
-        loss_q.backward()
-        print('grad',self.net.parameters()[0].grad)
-        # print('meta update')
-        # for p in self.net.parameters()[:5]:
-        # 	print(torch.norm(p).item())
-        self.meta_optim.step()
+        del metagrad
+        print('beforepara',self.net.varstest[0])
+        print('beforepara',self.net.varstest[13])
+        newpara=list(map(lambda p: p[0]-self.meta_lr*p[1], zip(self.net.varstest,up_grad)))
+        del up_grad
+        print('newpara',newpara[0])
+        print('newpara',newpara[13])
+        self.net.model.setpara(newpara,False)
+        self.net.varstest=self.net.model.weight_bias()
         print('afterpara',self.net.parameters()[0])
+        print('afterpara',self.net.parameters()[13])
 
         accs = np.array(corrects) / (task_num)
         return accs,loss_q
